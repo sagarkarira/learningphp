ad619b7cb9c998b607094531c4fc0ac30cd49649
SAGAR KARIRA <sagarkarira1992@gmail.com>
2013-07-17 21:58:43 +0530

    the first commit i did was all wrong .This is the correct code

--- replace_app.php
+++ replace_app.php
@@ -1,15 +1,24 @@
 <?php
-	
-			echo$sentence = $_POST["sentence"];
-	echo $word = $_POST["word"];
-	echo $replace = $_POST["replace"];
-
-
+	$new_sentence="";
+	if (isset($_POST['sentence'])&&!empty($_POST['sentence']))
+	{
+	 $sentence=$_POST["sentence"];
+	 $word=$_POST["word"];
+	  $replace=$_POST["replace"];
+	  
+	  $position=strpos($sentence,$word);
+	  
+	  
+	  
+	  $new_sentence=substr_replace($sentence,$replace,$position, strlen($word));
+	  
+	 } 
 ?>
-<hr>
- <form action="welcome.php" method="post">
-	<textarea name="sentence"  rows="6" cols="30" ></textarea></br></br>
-	Enter the word to be replaced:		<input name="word" type="text"></br>
-	Enter the word to be replaced with: <input name="replace" type="text"></br>
+
+<form action="welcome.php" method="POST">
+	Enter the sentence you want to modify :</br><textarea name="sentence"  rows="6" cols="30" ><?php echo $new_sentence; ?></textarea></br></br>
+	Enter the word to be replaced:	</br>	<input name="word" type="text"></br>
+	Enter the word to be replaced with:</br> <input name="replace" type="text"></br></br>
 	<input type="submit" value="Submit">
 	</form>
+ 
